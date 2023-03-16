aebcfb4cd26b2d2e7d7660f169cb74a15db10e56
Saurabh Jhingan <saurabh.jhingan.08@gmail.com>
2023-03-16 15:45:06 +0530

    Updated Return message for notification

--- application.py
+++ application.py
@@ -4,4 +4,4 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return 'Hello World. Updating the message for the world.'
+    return 'Hello World. Updating the message for the world. This message is from your Lord, Hensen Dumfry.'
